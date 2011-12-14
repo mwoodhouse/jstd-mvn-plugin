74c73f0b356e1dd44a2be4d56da6d4e22bad4c5e
mwoodhouse <mwoodhouse@qmetric.co.uk>
2011-12-14 17:44:14 +0000

    First build of plugin

---
+++ src/test/java/com/jstd/JSTDMvnPluginTest.java
@@ -0,0 +1,19 @@
+package com.jstd;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.junit.Test;
+
+public class JSTDMvnPluginTest
+{
+    @Test
+    public void shouldRunTests() throws MojoExecutionException, MojoFailureException
+    {
+        JSTDMvnPlugin jstdMvnPlugin = new JSTDMvnPlugin();
+
+        jstdMvnPlugin.setConfigFilePath("/Users/mwoodhouse/projects/jstd-mvn-plugin/runner.conf");
+
+        jstdMvnPlugin.execute();
+    }
+
+}
